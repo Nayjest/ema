630be7e014513adb04bc0887b50d287166a2641a
Nayjest <mail@vitaliy.in>
2025-03-23 22:30:00 +0100

    EMA-12: incremental reindexing, VectorDB usage

--- ema/db.py
+++ ema/db.py
@@ -1,17 +1,45 @@
 import os
+import logging
+
+import pymysql
+import sqlalchemy
 from sqlalchemy import Engine, MetaData, create_engine
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
+from microcore import ui
 
 session: sessionmaker
 db_engine: Engine
 db_metadata: MetaData
 
-def init_db():
+def init_db(verbose=False):
     global session, db_engine, db_metadata
-    db_engine = create_engine(os.getenv("DB_URL"), echo=True, future=True)
+
+
+    db_engine = create_engine(os.getenv("DB_URL"), echo=verbose, future=True)
     session = sessionmaker(bind=db_engine)
     db_metadata = MetaData()
+    # logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
+    check_db_connection()
 
+
+def check_db_connection():
+    print("Checking database connection... ", end='')
+    try:
+        with session() as ses:
+            ses.execute(text("SELECT 1")).fetchall()
+    except sqlalchemy.exc.OperationalError as e:
+        print(ui.red(str(e)))
+        exit()
+    print(f"[{ui.green('OK')}]")
+
+def sql(query: str):
     with session() as ses:
-        print(ses.execute(text("SELECT 1")).fetchall())
+        result = ses.execute(text(query))
+        try:
+            rows = result.mappings().all()
+        except Exception:
+            rows = None
+        ses.commit()
+    return rows
--- ema/env.py
+++ ema/env.py
@@ -1,26 +1,63 @@
 import os
+import sys
+import time
 
 import dotenv
-
-from ema.linear_api import LinearApi, LinearConfig
 import microcore as mc
+from chromadb.utils import embedding_functions
 from microcore import ui
+
+import ema
 from ema.db import init_db
+from ema.linear_api import LinearApi, LinearConfig
+from colorama import Fore, Style
 
 DEFAULT_ENV_FILE = ".env.mi"
 linear_api: LinearApi
 
+def print_logo():
+    green = Fore.GREEN  # For borders
+    bright = Style.BRIGHT
+    white = Fore.WHITE
+    logo = [
+        f"{green}{bright}"
+        f"╔══════════════════════════════════════════════════╗\n",
+        f"║                                                  ║\n",
+        f"║            ███████╗███╗   ███╗ █████╗            ║\n",
+        f"║            ██╔════╝████╗ ████║██╔══██╗           ║\n",
+        f"║            █████╗  ██╔████╔██║███████║           ║\n",
+        f"║            ██╔══╝  ██║╚██╔╝██║██╔══██║           ║\n",
+        f"║            ███████╗██║ ╚═╝ ██║██║  ██║           ║\n",
+        f"║            ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝           ║\n",
+        f"║                                                  ║\n",
+        f"║  --==<[ {Fore.RESET}Engineering Manager Assistant AI{green}{bright} ]>==--  ║\n",
+        f"{green}{bright}"
+        f"║                      {green}v{ema.__version__}{green}{bright}                      ║\n",
+        f"║                                                  ║\n",
+        f"{green}{bright}"
+        f"╚══════════════════════════════════════════════════╝"
+    ]
+
+    print("".join(logo))
 
 def bootstrap(env_file: str = DEFAULT_ENV_FILE):
+    print_logo()
     if env_file:
         dotenv.load_dotenv(env_file)
+
     global linear_api
     print(f"Bootstrapping... {ui.gray('env: ')}{ui.green(env_file)}")
+    if sys.platform == 'win32':
+        dotenv.load_dotenv(".env.win_override", override=True)
+        print(f"\t{ui.gray('env override: ')}{ui.green('.env.win_override')}")
+
     linear_api = LinearApi(LinearConfig())
     mc.configure(
-        USE_DOT_ENV=True,
-        DOT_ENV_FILE=env_file,
+        USE_DOT_ENV=False,
         USE_LOGGING=True,
+        EMBEDDING_DB_FUNCTION=embedding_functions.SentenceTransformerEmbeddingFunction(
+            model_name="paraphrase-multilingual-MiniLM-L12-v2"
+        )
     )
     init_db()
 
